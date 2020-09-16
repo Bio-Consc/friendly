4745d97cd14abe0fadf8889767a4b68a441983d5
backmeupplz <backmeupplz@Nikitas-MacBook-Pro-2.local>
2020-09-16 16:03:11 -0700

    update

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@
   v-layout.text-xs-center(column, justify-center, align-center)
     v-flex(xs12, md10)
       .headline.pb-4
-        span {{ $t("notFound") }}
+        span Not found
 </template>
 
 <script lang="ts">
